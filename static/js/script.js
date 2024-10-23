ad36eab5da4299c384d3eee1eaf9ccc404f720ee
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-24 00:13:58 +0200

    javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.menu');
 const dropDown = document.querySelector('.show-menu');
 
+// a function for the menu button
 menuButton.addEventListener("click", () => {
   if (dropDown.style.display === "flex") {
     dropDown.style.display = "none";
